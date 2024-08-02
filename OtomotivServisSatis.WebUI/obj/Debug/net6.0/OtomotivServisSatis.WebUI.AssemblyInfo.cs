449dcc0d30c8ed9e57e2733450ad49a573a3eb3f
Berkayalpay <berkayalpay08@gmail.com>
2024-08-02 18:50:02 +0300

    First Commit

--- OtomotivServisSatis.WebUI/obj/Debug/net6.0/OtomotivServisSatis.WebUI.AssemblyInfo.cs
+++ OtomotivServisSatis.WebUI/obj/Debug/net6.0/OtomotivServisSatis.WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OtomotivServisSatis.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efef4a5dd8331c3ab9e240adf3ab0ad3598dc856")]
 [assembly: System.Reflection.AssemblyProductAttribute("OtomotivServisSatis.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OtomotivServisSatis.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
